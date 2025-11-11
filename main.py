ca8d2612cf1e503be38d111d12c0d7123aa2231c
luttrelldb <luttrelldb@gmail.com>
2025-11-11 12:41:54 -0500

    Prettied up the output a lot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, count_characters
+from stats import get_word_count, count_characters, sort_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,11 +7,17 @@ def get_book_text(file_path):
 
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     char_counts = count_characters(book_text)
-    print(char_counts)
-
+    sorted_chars = sort_characters(char_counts)
+    print("--------- Character Count -------")
+    for char in sorted_chars:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -10,3 +10,9 @@ def count_characters(text):
         else:
             characters[char] = 1
     return characters
+
+def sort_characters(characters):
+    list_of_dicts = []
+    for char, count in characters.items():
+        list_of_dicts.append({"char": char, "num": count})
+    return sorted(list_of_dicts, key=lambda x: x['num'], reverse=True)
